3d8093c99939d1e14be7b6ead1f28bfb4f99dbad
Hüseyin Erkal <66094997+hsynerkl@users.noreply.github.com>
2022-03-21 17:17:07 +0300

    tesla

---
+++ src/component/Section.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+
+const Section = ({ title, description, leftBtnText, rightBtnText, backgroundImg }) => {
+
+    return (
+        <Wrap bgImage={backgroundImg}>
+
+            <ItemText>
+
+                <h1>{title}</h1>
+                <p>{description}</p>
+
+            </ItemText>
+
+            <Buttons>
+                <ButtonGroup>
+                    <LeftButton>
+                        {leftBtnText}
+                    </LeftButton>
+                    {rightBtnText &&
+                        <RightButton>
+                            {rightBtnText}
+                        </RightButton>
+                    }
+
+                </ButtonGroup>
+                <DownArrow src="/images/down-arrow.svg" />
+            </Buttons>
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div`
+    z-index:10;
+    width:100vw;
+    height:100vh;
+    background-image:url('./images/${props => props.bgImage}');
+    background-size:cover;
+    background-position:center;
+    background-repeat:no-repeat;
+    display:flex;
+    flex-direction:column;
+    justify-content:space-between;
+    align-items:center;
+`
+const ItemText = styled.div`
+    padding-top:15vh;
+    text-align:center;
+`
+const ButtonGroup = styled.div`
+   display:flex;
+   margin-bottom:30px;
+   @media (max-width:768px){
+       flex-direction:column;
+   }
+`
+const LeftButton = styled.div`
+   background-color:rgba(23,26,32,0.8);
+   height:40px;
+   width:256px;
+   color:white;
+   display:flex;
+   justify-content:center;
+   align-items:center;
+   border-radius:100px;
+   opacity: 0.80;
+   text-transform:uppercase;
+   font-size:12px;
+   cursor:pointer;
+   margin:8px;
+`
+
+const RightButton = styled(LeftButton)`
+    background-color:white;
+    color:black;
+    opacity:0.6;
+`
+const DownArrow = styled.img`
+    height:40px;
+    overflow-x:hidden;
+    animation:animateDown infinite 1.5s;
+`
+const Buttons = styled.div`
+    display:flex;
+    flex-direction:column;
+    cursor:pointer;
+`
+
